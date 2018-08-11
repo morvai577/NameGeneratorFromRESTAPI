aee3cb998b667cb9777a0b1addb24ce2bccb8965
Vaibhav Mor <mor.vaibhav577@gmail.com>
2018-08-12 11:44:43 +1200

    Add AJAX call

--- js/app.js
+++ js/app.js
@@ -23,5 +23,35 @@ function loadNames(e) {
     if(amount !== '') {
         url += `amount=${amount}&`;
     }
-    console.log(url);
+
+    // Ajax call
+    const xhr = new XMLHttpRequest();
+
+    // Open the connection
+    xhr.open('GET', url, true);
+
+    // Execute the function
+    xhr.onload = function () {
+        if (this.status === 200) {
+            const names = JSON.parse(this.responseText);
+
+            // Insert into the HTML
+
+            let html = '<h2>Generated Names</h2>';
+            html += '<ul class="list">';
+            names.forEach(function (name) {
+                html += `
+                    <li>${name.name}</li>
+                `;
+            });
+            html += '<ul>';
+
+            document.querySelector('#result').innerHTML = html;
+
+
+        }
+    };
+
+    // Send the request
+    xhr.send();
 }
